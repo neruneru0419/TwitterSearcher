097410b009d6410df12cb9ef92ece2197fa94403
neruneru0419 <neruneru55555@gmail.com>
2020-10-21 11:49:20 +0900

    設定ファイルを変更

--- Python/app.py
+++ Python/app.py
@@ -121,7 +121,7 @@ def get_tweet():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return render_template("index.html")
+    return "hof"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
