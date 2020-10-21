1ebd43d0152ac7209bfc77cb807c79e61285285b
neruneru0419 <neruneru55555@gmail.com>
2020-10-21 15:44:20 +0900

    change callback url

--- Python/app.py
+++ Python/app.py
@@ -32,7 +32,7 @@ app.secret_key = os.urandom(12)
 CORS(app)
 consumer_key = os.getenv("consumer_key")
 consumer_secret = os.getenv("consumer_secret")
-oauth_callback = "https://neruneru-twirrer-searcher.herokuapp.com/getapikey"
+oauth_callback = "https://neruneru-twitter-searcher.herokuapp.com/getapikey"
 tw_oauth = TwitterOAuth(consumer_key, consumer_secret, oauth_callback)
 @app.route("/oauth")
 def oauth_app():
