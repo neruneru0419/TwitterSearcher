a3bed98e70e0aa3f1da23eca5ce4fed331167c2e
neruneru0419 <neruneru55555@gmail.com>
2020-10-24 22:35:52 +0900

    デザインをレスポンシブに調整中

--- Vue/src/components/Header.vue
+++ Vue/src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
   <b-navbar toggleable="sm" type="dark" variant="info" class=shadow >
     <b-navbar-brand href="/">Tweet Searcher</b-navbar-brand>
-
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item active href="/oauth"><font-awesome-icon :icon="['fab','twitter']"/>ログイン</b-nav-item>
@@ -10,15 +10,40 @@
         <b-nav-item active to="/tweetsearch">ツイート検索</b-nav-item>
         
       </b-navbar-nav>
-
+    </b-collapse>
       <b-navbar-nav class="ml-auto">
         <b-nav-item active href="https://github.com/neruneru0419/TwitterSearcher"><font-awesome-icon :icon="['fab','github']"/></b-nav-item>
       </b-navbar-nav>
 
-    </b-collapse>
+    
   </b-navbar>
-</div>
+  </div>
+  <!-- <div class="myheader">  
+    <b-navbar toggleable="sm" type="dark" variant="info">
+      <b-navbar-brand to=“/“>HOME</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/about">about</b-nav-item>
+          <b-nav-item to="/game">game</b-nav-item>
+          <b-nav-item to="/paint">paint</b-nav-item>
+          <b-nav-item to="/scrap">scrap</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item active href="https://github.com/neruneru0419/TwitterSearcher"><font-awesome-icon :icon="['fab','github']"/></b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div> -->
 </template>
+
+<style>
+h1{
+  margin: 0 auto;
+}
+</style>
+
+  
 
 <style>
 h1{
