a85dea46ebd36359eb7d09676f0a89832608d856
neruneru0419 <neruneru55555@gmail.com>
2020-11-21 18:23:15 +0900

    fix title

--- Vue/src/components/Login.vue
+++ Vue/src/components/Login.vue
@@ -3,8 +3,8 @@
     <Header></Header>
     <b-container>
         <h1>Tweet Searcher</h1>
-      <p>TwitterSearcherはTwitterのフォロワーだったりツイートの検索ツールです。</p>
-      <p>このサイトはTwitterアカウントが必要です。</p>
+        <p>TwitterSearcherはTwitterのフォロワーだったりツイートの検索ツールです。</p>
+        <p>このサイトはTwitterアカウントが必要です。</p>
         <h2>フォロワー検索</h2>
         <p>対象のアカウントのフォロワーについて調べることができます。</p>
         <p><b-button variant="primary" href='/oauth?redirectURL=/followersearch'>ログインしてフォロワー検索を始める</b-button></p>
